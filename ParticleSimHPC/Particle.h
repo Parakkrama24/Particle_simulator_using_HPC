bf2f248c706d06e98db3ed603b4a58f52f9a5bbf
dewbulegoda20 <dew.bulegoda20@gmail.com>
2026-05-24 07:17:31 +0530

    Modified Particle.h

--- ParticleSimHPC/Particle.h
+++ ParticleSimHPC/Particle.h
@@ -7,6 +7,7 @@ struct Particle {
     double x, y, z;        // Position
     double vx, vy, vz;     // Velocity
 
+
     Particle(double x, double y, double z,
         double vx, double vy, double vz)
         : x(x), y(y), z(z), vx(vx), vy(vy), vz(vz) {
